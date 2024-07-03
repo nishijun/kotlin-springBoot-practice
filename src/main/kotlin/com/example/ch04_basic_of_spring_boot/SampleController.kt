94b2e5f40ca9f3f4a8906dd60b16d6e87923a6f9
Junya Nishiwaki <junya4318@gmail.com>
2024-07-03 09:36:45 +0900

    first commit

---
+++ src/main/kotlin/com/example/ch04_basic_of_spring_boot/SampleController.kt
@@ -0,0 +1,11 @@
+//package com.example.ch04_basic_of_spring_boot
+//
+//import org.springframework.http.ResponseEntity
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//@RestController
+//class SampleController {
+//    @GetMapping("/")
+//    fun getPerson(): ResponseEntity<Person>
+//}
---
+++ src/test/kotlin/com/example/ch04_basic_of_spring_boot/Ch04BasicOfSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ch04_basic_of_spring_boot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Ch04BasicOfSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
